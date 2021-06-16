8d65509aa11c078f898f960ceb9a31238154231d
rahalrazika <rahalraika@gmail.com>
2021-06-16 15:24:47 +0200

    fix store error with reducer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+
 import './style/index.css';
 import App from './components/App';
-import rootReducer from './reducers/index';
+import store from './reducers/store';
 
-const initialState = [
-  { id: Math.random(), title: 'The Call of the Wild', category: 'Action' },
-  { id: Math.random(), title: 'To Kill a Mockingbird', category: 'Biography' },
-  { id: Math.random(), title: 'And Then There Were None', category: 'History' },
-  { id: Math.random(), title: 'Ninth House', category: 'Horror' },
-  { id: Math.random(), title: 'Clean Code', category: 'Learning' },
-
-];
-const store = createStore(rootReducer, initialState);
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,14 @@
 const CREATE_BOOK = 'CREATE_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
-const bookReducer = (state, action) => {
+const initialState = [
+  { id: Math.random(), title: 'The Call of the Wild', category: 'Action' },
+  { id: Math.random(), title: 'To Kill a Mockingbird', category: 'Biography' },
+  { id: Math.random(), title: 'And Then There Were None', category: 'History' },
+  { id: Math.random(), title: 'Ninth House', category: 'Horror' },
+  { id: Math.random(), title: 'Clean Code', category: 'Learning' },
+
+];
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
       return [...state, action.payload];
